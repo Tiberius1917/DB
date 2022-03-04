ea56e6787742746af93105527c0d77fd5261a644
Tiberius <trdownie@gmail.com>
2022-03-04 19:00:38 +0000

    Updated settings

--- demandbetter/settings.py
+++ demandbetter/settings.py
@@ -121,7 +121,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-DISABLE_COLLECTSTATIC = 1
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
