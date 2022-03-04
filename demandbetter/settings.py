a3aa0810943424ecf8001031004570b029986161
Tiberius <trdownie@gmail.com>
2022-03-04 18:42:13 +0000

    Updated collectstatic settings for heroku

--- demandbetter/settings.py
+++ demandbetter/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+DISABLE_COLLECTSTATIC = 1
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
